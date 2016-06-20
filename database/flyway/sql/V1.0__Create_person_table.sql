1863f5b1664ec0b585acda1ab75d79a458a51ea0
Divan Visagie <visagiedivan@gmail.com>
2016-06-20 19:41:26 +0200

    put bcrypt into migration

--- database/flyway/sql/V1.0__Create_person_table.sql
+++ database/flyway/sql/V1.0__Create_person_table.sql
@@ -6,4 +6,4 @@ CREATE TABLE users
   CONSTRAINT pk PRIMARY KEY (id)
 );
 
-insert into users(username,password_hash) values ('john','johns-hash'), ('mary','marys-hash');
+insert into users(username,password_hash) values ('bob','$2a$10$uIXsjBLBvguLkg6SXGi/JOyHR92hkwUR43vwIcQdRO4ac.FwKbwgi'), ('mary','marys-hash');
