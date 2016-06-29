180c7d43c954086613e6420ffb5a7fef22f45c9c
Divan Visagie <visagiedivan@gmail.com>
2016-06-29 21:12:07 +0200

    add tracing to http client

--- test-client/src/main/scala/com/sgtest/TestClientServer.scala
+++ test-client/src/main/scala/com/sgtest/TestClientServer.scala
@@ -3,7 +3,10 @@ package com.sgtest
 import java.net.InetSocketAddress
 
 import com.sgtest.controllers.AuthenticationController
+import com.twitter.finagle.Http
 import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finagle.stats.DefaultStatsReceiver
+import com.twitter.finagle.zipkin.thrift.ZipkinTracer
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
 import com.twitter.finatra.http.routing.HttpRouter
@@ -23,6 +26,18 @@ class TestClientServer extends HttpServer {
       .filter[TraceIdMDCFilter[Request, Response]]
       .filter[CommonFilters]
       .add[AuthenticationController]
+  }
+
+  override def configureHttpServer(server: Http.Server): Http.Server = {
+    val receiver = DefaultStatsReceiver.get
+    val tracer = ZipkinTracer.mk(
+      host = "localhost",
+      port = 9410,
+      statsReceiver = receiver,
+      sampleRate = 1.0f
+    )
+    server
+        .withTracer(tracer)
   }
 
 }
