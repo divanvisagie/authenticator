7a1ca17a7e27b6d2aa694ffb02a6426a4700a006
Divan Visagie <visagiedivan@gmail.com>
2016-07-10 15:41:36 +0200

    fix

--- masque/src/main/scala/com/sgtest/controllers/AuthenticationController.scala
+++ masque/src/main/scala/com/sgtest/controllers/AuthenticationController.scala
@@ -3,23 +3,22 @@ package com.sgtest.controllers
 import javax.inject.Inject
 import com.sgtest.services.UserService
 import com.twitter.finatra.http.Controller
-
-
+import com.swissguard.authentication.thriftscala.{RegistrationRequest, LoginRequest}
 
 class AuthenticationController @Inject()(userService: UserService)
   extends Controller {
 
-//  post("/register") { request:  =>
-//    userService.registerUser(request.toThrift) handle {
-//      case _: Exception => response.status(420)
-//    }
-//  }
-//
-//  post("/login") { request: RegisterUserRequest =>
-//    userService.login(request.toThrift) handle {
-//      case e: Exception => response.status(401)
-//        response.unauthorized( e.getMessage)
-//    }
-//  }
+  post("/register") { request: RegistrationRequest =>
+    userService.registerUser(request) handle {
+      case _: Exception => response.status(420)
+    }
+  }
+
+  post("/login") { request: LoginRequest =>
+    userService.login(request) handle {
+      case e: Exception => response.status(401)
+        response.unauthorized( e.getMessage)
+    }
+  }
 
 }
